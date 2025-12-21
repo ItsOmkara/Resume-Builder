1d41b383553fbf6c1fda04db3c6c9d29aaf771f9
itsOmkara <9403139921om@gmail.com>
2025-12-22 00:15:28 +0530

    Initial Spring Boot project

---
+++ src/test/java/in/omkarbirajdar/resume_builder/ResumeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package in.omkarbirajdar.resume_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
