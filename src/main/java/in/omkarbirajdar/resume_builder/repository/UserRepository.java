589b171e77b5385107af01e808bdf5f6b41d4e76
itsOmkara <9403139921om@gmail.com>
2025-12-24 22:55:42 +0530

    Add authService

--- src/main/java/in/omkarbirajdar/resume_builder/repository/UserRepository.java
+++ src/main/java/in/omkarbirajdar/resume_builder/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package in.omkarbirajdar.resume_builder.repository;
 
-public interface UserRepository {
+import in.omkarbirajdar.resume_builder.document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User,String> {
+
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+
 }
