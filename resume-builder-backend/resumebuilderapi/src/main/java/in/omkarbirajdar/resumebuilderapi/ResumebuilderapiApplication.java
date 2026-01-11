c763acd74c791cec147a4b4fda6e5585398c7dc7
itsOmkara <9403139921om@gmail.com>
2026-01-12 01:39:22 +0530

    Initial commit

---
+++ resume-builder-backend/resumebuilderapi/src/main/java/in/omkarbirajdar/resumebuilderapi/ResumebuilderapiApplication.java
@@ -0,0 +1,12 @@
+package in.omkarbirajdar.resumebuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumebuilderapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ResumebuilderapiApplication.class, args);
+	}
+
+}
---
+++ resume-builder-backend/resumebuilderapi/src/main/java/in/omkarbirajdar/resumebuilderapi/exception/ResourceExistsException.java
@@ -0,0 +1,8 @@
+package in.omkarbirajdar.resumebuilderapi.exception;
+
+public class ResourceExistsException extends RuntimeException{
+
+    public ResourceExistsException(String message) {
+        super(message);
+    }
+}
